e499a086503a58100633c25dbde8e9e8b49db914
evgenii <evgeniikriuchkov@gmail.com>
2024-03-20 21:24:26 +0100

    Add funcs to create and to add user

---
+++ developers/src/api_v1/card/crud.py
@@ -0,0 +1,24 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.engine import Result
+from fastapi_pagination import paginate, Page
+
+from src.api_v1.user.schemas import UserCreate
+from src.models import User
+
+
+async def get_users(session: AsyncSession) -> Page[User]:
+    stmt = select(User).order_by(User.created_at)
+    result: Result = await session.execute(stmt)
+    users = result.scalars().all()
+    return paginate(users)
+
+
+async def create_user(
+    session: AsyncSession,
+    user_in: UserCreate,
+) -> User:
+    user = User(**user_in.model_dump())
+    session.add(user)
+    await session.commit()
+    return user
